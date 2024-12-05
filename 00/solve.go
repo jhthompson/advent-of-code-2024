e0f39c34e34a4a0d57d4fb0edbff06b7015a26a3
Jeremy Thompson <jeremythompson.918@gmail.com>
2024-12-04 19:57:22 -0500

    Add basic template

---
+++ 00/solve.go
@@ -0,0 +1,19 @@
+package zero
+
+import (
+	"log/slog"
+)
+
+func SolvePartOne(lines []string) int {
+	result := 0
+
+	slog.Info("00.0", "result", result)
+	return result
+}
+
+func SolvePartTwo(lines []string) int {
+	result := 0
+
+	slog.Info("00.0", "result", result)
+	return result
+}
